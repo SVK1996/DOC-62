01ddee09ef9e549cd99320f4e8bf66a47811da4d
Harish (Raaho) <harish@raaho.in>
2021-06-05 22:26:52 +0530

    Create select-all.sql

---
+++ db/sql/select-all.sql
@@ -0,0 +1,13 @@
+-- Query all columns (attributes) for every row in the CITY table..
+
+--The CITY Table is described as follows
+--Field       | Type
+---------------------------
+--ID          | NUMBER
+--NAME        | VARCHAR2(17)
+--COUNTRYCODE | VARCHAR2(3)
+--DISTINCT    | VARCHAR2(20)
+--POPULATION  | NUMBER
+
+SELECT * FROM CITY;
+
